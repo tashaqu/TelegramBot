a4d7336c969ab7834c16e10417cf1ad7109ec03e
tashaqu <dubudub@yahoo.com>
2022-11-19 11:01:00 +0300

    TelegramBOT creation

---
+++ Exceptions.py
@@ -0,0 +1,31 @@
+import requests
+import json
+from config import keys
+
+class APIException(Exception):
+    pass
+class MoneyConverter:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+
+
+        if quote == base:
+            raise APIException(f'Не удалось перевести одинаковые валюты {base}.')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {base}')
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = float(json.loads(r.content)[base_ticker]) * amount
+        return total_base
---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN ="5844948411:AAHqluPJJsUhrVILD6kj3Qug2GgTCxk4-ds"
+
+keys={
+    "рубль": "RUB",
+    "евро": "EUR",
+    "доллар": "USD"
+}
